413082ef58ff868a1edd970c5a8850d849533625
gastin11 <gastinalfiansa@gmail.com>
2024-03-17 19:49:36 +0700

    first commit

---
+++ index-script.js
@@ -0,0 +1,19 @@
+let popup = document.getElementById("popup-cara"),
+    popup2 = document.getElementById("popup-tentang");
+
+function openPopupCara(){
+        popup.classList.add("open-popup-cara");
+}
+    
+function closePopupCara(){
+        popup.classList.remove("open-popup-cara");
+}
+    
+
+function openPopupTentang(){
+    popup2.classList.add("open-popup-tentang");
+}
+
+function closePopupTentang(){
+    popup2.classList.remove("open-popup-tentang");
+}
---
+++ script.js
@@ -0,0 +1,105 @@
+let popup = document.getElementById("popup")
+var gerak = true;
+var meteorSpeed = 0.5;
+
+function openPopup(){
+    var result = document.getElementById("result");
+    var score = document.getElementById('score').innerHTML;
+    result.textContent = score;
+    popup.classList.add("open-popup");}
+
+function closePopup(){
+    popup.classList.remove("open-popup");
+    location.reload();
+}
+
+document.getElementById("menuButton").addEventListener("click", function() {
+    window.location.href = "index.html";
+});
+
+function setBackgroundMoving() {
+    if (gerak == true) {
+        setTimeout(function () {
+            // setting background gerak
+            var bg = document.getElementById('main');
+            bg.style.backgroundPositionY = (parseInt(bg.style.backgroundPositionY.replace('px', '')) + 1) + 'px';
+
+            // skor
+            var scoreElement = document.getElementById('score');
+            var score = parseInt(scoreElement.innerHTML);
+            scoreElement.innerHTML = score + 1;
+
+            // Periksa apakah skor baru mencapai kelipatan 1000
+            if (score % 1500 === 0) {
+                meteorSpeed += 1; // Tingkatkan kecepatan pergerakan meteor
+            }
+
+            // panggil rekursif setbackgroundmoving
+            setBackgroundMoving();
+        }, 3);
+    }
+}
+
+// inisialisasi background gerak
+setBackgroundMoving();
+
+// seting meteor
+function setMeteorMoving() {
+    var meteor = document.getElementById('meteor'),
+        plane = document.getElementById('plane');
+
+    setTimeout(function () {
+
+        meteor.style.marginTop = (parseInt(meteor.style.marginTop.replace('px', '')) + meteorSpeed) + 'px'; // Menggunakan meteorSpeed untuk mengatur kecepatan pergerakan meteor
+
+        if (parseInt(meteor.style.marginTop.replace('px', '')) > 500) {
+            meteor.style.marginLeft = (Math.floor(Math.random() * 250) + 50) + 'px';
+            meteor.style.marginTop = '-100px';
+        }
+
+        if (meteor.offsetTop + 56 >= plane.offsetTop && meteor.offsetLeft + 50 >= plane.offsetLeft && meteor.offsetTop + 56 <= plane.offsetTop + 100 && meteor.offsetLeft <= plane.offsetLeft + 50) {
+            openPopup();
+            gerak = false;
+            plane.setAttribute('class', 'freeze');
+            meteor.setAttribute('class', 'freeze');
+        } else {
+            //meteor gerak
+            setMeteorMoving();
+        }
+
+    }, 10);
+}
+
+// inisialisasi meteor
+setMeteorMoving();
+
+window.addEventListener('keydown',function(e){
+    var plane = document.getElementById('plane'),
+        kiri = parseInt(plane.style.marginLeft.replace('px',''));
+        atas = parseInt(plane.style.marginTop.replace('px',''));
+    /*
+    37 kiri
+    38 atas
+    39 kanan
+    40 bawah
+    */ 
+    if(gerak == true){
+        if(e.keyCode == 37){
+            if(kiri > 0){
+                plane.style.marginLeft = (kiri - 10) + 'px';
+            }
+        }else if(e.keyCode == 38){
+            if(atas > 0){
+                plane.style.marginTop = (atas - 10) + 'px';
+            }
+        }else if(e.keyCode == 39){
+            if(kiri < 340){
+                plane.style.marginLeft = (kiri + 10) + 'px';
+            }
+        }else if(e.keyCode == 40){
+            if(atas < 400){
+                plane.style.marginTop = (atas + 10) + 'px';
+            }
+        }
+    }
+});
